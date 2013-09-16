b7c3ab52e0abd9f9067fd7180f7a895d6f6436f0
jonny928 <jonny928@student.liu.se>
2013-09-16 14:33:22 +0200

    First commit

---
+++ src/se/liu/ida/jonny928/tddc70/closestsums/Main.java
@@ -0,0 +1,11 @@
+package se.liu.ida.jonny928.tddc70.closestsums;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jonathan
+ * Date: 2013-09-16
+ * Time: 14:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
